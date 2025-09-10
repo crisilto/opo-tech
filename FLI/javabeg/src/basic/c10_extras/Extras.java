5a76d4fe628bfc2451c990f372a4a98b9285a5e6
Crisilto <cristinasilvestre1708@gmail.com>
2025-09-10 14:10:02 +0200

    update

--- FLI/javabeg/src/basic/c10_extras/Extras.java
+++ FLI/javabeg/src/basic/c10_extras/Extras.java
@@ -1,4 +1,14 @@
 package basic.c10_extras;
 
+import java.util.Scanner;
+
 public class Extras {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.println("introduce tu nombre");
+        var name = scanner.nextLine();
+        System.out.println("introduce tu edad");
+        var age = scanner.nextInt();
+        System.out.println("nombre: "+name+", edad: "+age);
+    }
 }
