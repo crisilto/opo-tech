3dcd7a77ac71e4b3c138ad1cafe0c2c1bd51e92b
Crisilto <cristinasilvestre1708@gmail.com>
2025-10-21 10:49:56 +0200

    python start

---
+++ FLI/python-beg/00_hello.py
@@ -0,0 +1,21 @@
+#comentario
+
+"""
+comentario 
+en varias lineas
+"""
+
+'''
+comentario 
+en varias lineas tb
+'''
+
+print("hi python")
+print('hi python')
+
+#consultar tipo de dato
+print(type("hi python")) #str
+print(type(8)) #int
+print(type(8.5)) #float
+print(type(True)) #bool
+print(type(2+1j)) #complex
---
+++ FLI/python-beg/01_hello_exercises.py
@@ -0,0 +1,61 @@
+# 1. Imprime "¡Hola Mundo!" por consola.
+print("¡Hola Mundo!")
+
+# 2. Escribe un comentario de una sola línea explicando qué
+# hace el código del Ejercicio 1.
+# ejecuta un string por consola
+
+# 3. Imprime tu nombre y edad en la misma línea utilizando la
+# función print.
+print("mi nombre es cris y tengo", 27, "años")
+
+# 4. Usa la función type() para imprimir el tipo de dato de
+# una cadena de texto, un número entero y un número
+# decimal.
+print(type("string"))
+print(type(13))
+print(type(13.11))
+
+
+# 5. Escribe un comentario en varias líneas explicando qué
+# son los tipos de datos en Python.
+"""
+los tipos de datos en python más comunes son:
+-str: cadena de texto
+-int: num entero
+-float: num decimal
+-bool: booleanos
+"""
+
+# 6. Imprime el resultado de concatenar dos cadenas de texto,
+# por ejemplo: "Hola" y "Mundo".
+print("hola", "mundo")
+
+# 7. Crea una variable para almacenar tu nombre, otra para tu
+# edad, e imprime ambas en la misma línea.
+my_name = "cris"
+my_age = 27
+print("me llamo", my_name, "y tengo", my_age, "años")
+
+# 8. Escribe un programa que solicite al usuario su nombre y
+# lo imprima junto con un saludo.
+user_name = input("¿cuál es tu nombre?")
+print("hola", user_name)
+
+# 9. Usa print() para mostrar el resultado de la suma de dos
+# números enteros y el tipo de dato resultante.
+
+# suma dos nums enteros
+result = 5+10
+# imprime el resultado de la suma
+print("el resultado es", result)
+# imprime el tipo de dato del resultado que es int
+print("el tipo de dato del resultado es", type(result))
+
+# 10. Comenta el código del Ejercicio 9, y explica qué
+# hace cada línea usando comentarios de una sola línea.
+
+print("Hola"+""+"Python")  # HolaPython
+print("Hola, Python")  # Hola, Python
+print("Hola", "Python")  # Hola Python
+print("Hola ", "Python")  # Hola  Python
